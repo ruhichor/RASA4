6819d193e9a839720fe12ab80541a9f9951d0efe
Asus <Asus@DESKTOP-QPEF4V1>
2020-08-19 00:04:19 +0530

    hello

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,7 +4,8 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("hello world");
+		System.out.println("hello wurld");
+		System.out.println("hello wurld");
 	}
 
 }
